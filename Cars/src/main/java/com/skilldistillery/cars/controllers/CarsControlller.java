fd4b31889f85755b93a53adf790cc275e8932f2c
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-11-08 23:02:12 -0700

    commit

--- Cars/src/main/java/com/skilldistillery/cars/controllers/CarsControlller.java
+++ Cars/src/main/java/com/skilldistillery/cars/controllers/CarsControlller.java
@@ -3,6 +3,7 @@ package com.skilldistillery.cars.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.skilldistillery.cars.data.CarsDAO;
 import com.skilldistillery.cars.entities.Cars;
 
+@Controller
 public class CarsControlller {
 
 	@Autowired
