4ee8345ed1b268de07f84da5a4227a3c604ff8c0
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-11-08 21:01:45 -0700

    commit

--- Cars/src/main/java/com/skilldistillery/cars/data/CarsDAO.java
+++ Cars/src/main/java/com/skilldistillery/cars/data/CarsDAO.java
@@ -6,15 +6,15 @@ import com.skilldistillery.cars.entities.Cars;
 
 public interface CarsDAO {
 	
-	public Cars findCarById();
+	public Cars findCarById(int id);
 	
-	public Cars findCarByKeyword();
+	public Cars findCarByKeyword(String keyword);
 	
-	public Cars addCar();
+	public Cars addCar(Cars car);
 	
-	public Cars deleteCar();
+	public Cars deleteCar(Cars car);
 	
-	public Cars updateCar();
+	public Cars updateCar(Cars car);
 	
 	public List<Cars> findAll();
 }
